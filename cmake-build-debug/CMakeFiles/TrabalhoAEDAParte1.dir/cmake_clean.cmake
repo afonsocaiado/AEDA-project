80bba52abf7c5646d165e6fc256d1d58c97c9fb8
afonsocaiado <afonsocaiado1@gmail.com>
2019-10-18 15:11:43 +0100

    TrabalhoTeste

---
+++ cmake-build-debug/CMakeFiles/TrabalhoAEDAParte1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrabalhoAEDAParte1.dir/main.cpp.o"
+  "TrabalhoAEDAParte1"
+  "TrabalhoAEDAParte1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrabalhoAEDAParte1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

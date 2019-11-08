7101e528f07d047fe9246c067f594f7e206df0f6
Tomás Gonçalves <tomasfgon@gmail.com>
2019-11-08 11:02:20 +0000

    addAtleta e removeAtleta implementados

--- prova.cpp
+++ prova.cpp
@@ -1,4 +1,5 @@
 #include "prova.h"
+#include "funcionario.h"
 
 #include <vector>
 #include <string>
@@ -21,3 +22,21 @@ void Prova::setData(int dia, int mes, int ano)
     data.setMes(mes);
     data.setAno(ano);
 }
+
+void Prova::addAtleta(Atleta &a) {
+    atletasParticipantes.push_back(a);
+}
+bool Prova::removeAtleta(Atleta &a) {
+    for(int i=0;i<atletasParticipantes.size();i++){
+        if (atletasParticipantes[i] == a){
+            atletasParticipantes.erase(atletasParticipantes.begin()+i);
+            cout << "Atleta removido com sucesso" << endl;
+            return 1;
+        }
+
+    }
+    cout << "Impossível remover atleta" << endl;
+    return 0;
+}
+
+

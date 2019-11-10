a9069ac5aa1084feb8d01dfd197f41d240b4c4c5
Zé Miguel Maçães <jose.macaes.2000@gmail.com>
2019-11-10 18:44:06 +0000

    Agora sim

--- util.cpp
+++ util.cpp
@@ -1,5 +1,12 @@
-//
-// Created by Fisica1 on 10/11/2019.
-//
-
 #include "util.h"
+
+using namespace std;
+
+string to_lower(string s)
+{
+    string newS;
+    for (unsigned int i = 0; i < s.size(); i++)
+        newS[i] = tolower(s[i]);
+
+    return newS;
+}
--- util.h
+++ util.h
@@ -1,19 +1,10 @@
 #ifndef TRABALHOAEDAPARTE1_UTIL_H
 #define TRABALHOAEDAPARTE1_UTIL_H
 
-#include <algorithm>
-#include <cctype>
 #include <string>
 
 using namespace std;
 
-string to_lower(string s)
-{
-    string newS;
-    for (unsigned int i = 0; i < s.size(); i++)
-        newS[i] = tolower(s[i]);
-
-    return newS;
-}
+string to_lower(string s);
 
 #endif //TRABALHOAEDAPARTE1_UTIL_H
